caf2b08887d0bafc207f58c39e934e51abd7cad1
Imabirdmf <vash.stampide@gmail.com>
2026-02-04 13:30:18 +0000

    Create Staff Model, url

---
+++ backend/planner/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Staff(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    priority = models.IntegerField(default=0)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def __str__(self):
+        return self.full_name
+
+class DaysOff(models.Model):
+    user = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    date = models.DateField()
+
+class Duty(models.Model):
+    date = models.DateField()
+
+class DutyAssignment(models.Model):
+    user = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    duty = models.ForeignKey(Duty, on_delete=models.CASCADE)
---
+++ backend/planner/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Staff
+
+class StaffSerializer(serializers.ModelSerializer):
+    full_name = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Staff
+        fields = ('id', 'email', 'last_name', 'first_name', 'full_name')
+
+
+
