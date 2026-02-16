f792f0e8f0439de3f2b224f313c244fab521117b
Imabirdmf <vash.stampide@gmail.com>
2026-02-16 15:29:04 +0000

    update

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -121,13 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/django/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://*.railway.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
