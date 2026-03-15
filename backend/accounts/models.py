06d0b80b242ba5e55150c94c8527b5714c883e39
Imabirdmf <vash.stampide@gmail.com>
2026-03-15 22:37:27 +0000

    fix superuser

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
     objects = UserManager()
 
     def __str__(self):
