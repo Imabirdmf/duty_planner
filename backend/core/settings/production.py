2092014fc57866d571d9d70bfd5aa3b81b527658
Imabirdmf <vash.stampide@gmail.com>
2026-03-15 22:24:33 +0000

    fix db

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -1,12 +1,15 @@
 import os
-
 import dj_database_url
-
 from .base import *  # noqa: F403
 
 DEBUG = False
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+
+
 ALLOWED_HOSTS = [
+    "dutyplannerfrontend-production.up.railway.app",
+    "dutyplannerbackend-production.up.railway.app",
+    "dutyplannerfrontend.railway.internal",
+    "dutyplannerbackend.railway.internal",
     "dutyplannerfrontend-staging.up.railway.app",
-    "https://dutyplannerfrontend-production.up.railway.app/",
+    "dutyplannerbackend-staging.up.railway.app"
 ]
