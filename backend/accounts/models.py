6723e40887b8dc55c46ac582bf0dc478e8c60fcd
Imabirdmf <vash.stampide@gmail.com>
2026-03-27 13:39:12 +0000

    fix custom user

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,37 @@
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.base_user import BaseUserManager
+from typing import Any
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email: str, password: str | None = None, **extra_fields: Any):
+        if not email:
+            raise ValueError("Please write an email")
+        email = self.normalize_email(email)
+        username = email.split("@")[0]
+        user = self.model(email=email, username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ):
+        super_user = self.create_user(email, password, **extra_fields)
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.save(using=self._db)
+        return super_user
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=150)
+    objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
