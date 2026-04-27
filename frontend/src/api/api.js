50b0e6df145493d3c69cb2480ebaab5c56b1fdc3
Imabirdmf <vash.stampide@gmail.com>
2026-04-27 10:56:24 +0100

    test

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+console.log(import.meta.env.VITE_API_URL)
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
